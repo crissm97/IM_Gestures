b440ea978ead95ec4d857a2be818dd89ca5fc952
Guilherme <guilhermesousa@ua.pt>
2022-01-16 19:25:41 +0000

    gestures final

--- AppGui/AppGui/MainWindow.xaml.cs
+++ AppGui/AppGui/MainWindow.xaml.cs
@@ -24,7 +24,11 @@ namespace AppGui
         private MmiCommunication mmiSender;
         private LifeCycleEvents lce;
         private MmiCommunication mmic;
-        private string game = "b4f2sYXl";
+        private string game = "";
+        
+        // Browser to deploy the game
+        private string browser = "firefox.exe";
+        //private string browser = "chrome.exe";
 
         // Lichess token - pako25
         static public string token = "lip_I0iYfH1quLT2GUWlxrAq";
@@ -56,23 +60,24 @@ namespace AppGui
             String action = null;
             switch ((string)json.recognized[1].ToString())
             {
-                case "giveup": action = "desisto";
+                case "resign": action = "resign";
                     break;
 
-                case "takeback": action = "engano";
+                case "draw": action = "draw";
                     break;
 
-                case "accept": action = "accengano";
+                case "reject": action = "reject";
                     break;
 
-                case "refuse": action = "rejengano";
+                case "sit": action = "sit";
                     break;
 
-                case "clap": action = "envia";
+                case "clapping": action = "clap";
                     break;
             }
 
-            if (action  == "desisto")
+            // If I pretend to give up
+            if (action  == "resign")
             {
                 Task<String> resign = Task.Run(() => Resign(game));
                 String result = resign.Result;
@@ -87,57 +92,44 @@ namespace AppGui
                 var exNot = lce.ExtensionNotification(0 + "", 0 + "", 1, result);
                 await mmic.Send(exNot);
             }
-            else if (action == "engano")
+            // If I pretend to propose a draw or accept a draw proposal
+            else if (action == "draw")
             {
                 string message = "yes";
-                Task<String> send_msg = Task.Run(() => TakeBack(game, message));
+                Task<String> send_msg = Task.Run(() => Draw(game, message));
                 String result = send_msg.Result;
                 string auxok = @"""ok""";
                 string auxtrue = "true";
                 string aux = "{" + auxok + ":" + auxtrue + "}";
                 if (result.Equals(aux))
                 {
-                    result = "{'ok' : 'Pedido enviado'}";
+                    result = "{'ok' : 'Empate'}";
                 }
                 await mmic.Send(lce.NewContextRequest());
                 var exNot = lce.ExtensionNotification(0 + "", 0 + "", 1, result);
                 await mmic.Send(exNot);
             }
-            else if (action == "accengano")
-            {
-                string message = "yes";
-                Task<String> send_msg = Task.Run(() => TakeBack(game, message));
-                String result = send_msg.Result;
-                string auxok = @"""ok""";
-                string auxtrue = "true";
-                string aux = "{" + auxok + ":" + auxtrue + "}";
-                if (result.Equals(aux))
-                {
-                    result = "{'ok' : 'Pedido aceite'}";
-                }
-                await mmic.Send(lce.NewContextRequest());
-                var exNot = lce.ExtensionNotification(0 + "", 0 + "", 1, result);
-                await mmic.Send(exNot);
-            }
-            else if (action == "rejengano")
+            // If I pretend to decline a draw proposal
+            else if (action == "reject")
             {
                 string message = "no";
-                Task<String> send_msg = Task.Run(() => TakeBack(game, message));
+                Task<String> send_msg = Task.Run(() => Draw(game, message));
                 String result = send_msg.Result;
                 string auxok = @"""ok""";
                 string auxtrue = "true";
                 string aux = "{" + auxok + ":" + auxtrue + "}";
                 if (result.Equals(aux))
                 {
-                    result = "{'ok' : 'Pedido rejeitado'}";
+                    result = "{'ok' : 'Empate'}";
                 }
                 await mmic.Send(lce.NewContextRequest());
                 var exNot = lce.ExtensionNotification(0 + "", 0 + "", 1, result);
                 await mmic.Send(exNot);
             }
-            else if (action == "envia")
+            // If I pretend to send a cheering chat message
+            else if (action == "clap") 
             {
-                string message = "Excelente jogada";
+                string message = "Excelente jogada!";
                 Task<String> send_msg = Task.Run(() => SendMessage(game, message));
                 String result = send_msg.Result;
                 string auxok = @"""ok""";
@@ -151,7 +143,68 @@ namespace AppGui
                 var exNot = lce.ExtensionNotification(0 + "", 0 + "", 1, result);
                 await mmic.Send(exNot);
             }
+            // If I pretend to challenge someone or accept challenge requests
+            else if (action == "sit")
+            {
+                Task<String> list = Task.Run(() => ListChallenges());
+                String result = list.Result;
+                Console.WriteLine(result);
+
+                string data = "";
+                if (result.Contains(@"""in"":[]"))
+                {
+                    data = "";
+                }
+                else
+                {
+                    data = getBetween(result, "id", "url");
+                    char[] removeStuff = { '"', ':', ',' };
+                    data = data.TrimStart(removeStuff);
+                    data = data.TrimEnd(removeStuff);
+                    game = data;
+                }
+
+                if (String.IsNullOrEmpty(data))
+                {
+                    Task<String> currentgames = Task.Run(() => SeeCurrentGames());
+                    String current = currentgames.Result;
+                    if (current.Contains(@"""nowPlaying"":[]"))
+                    {
+                        string user = "TestarCaderno";
+                        Task<String> challenge = Task.Run(() => Challenge(user));
+                        string challengeresult = challenge.Result;
+                        string challengedata = getBetween(challengeresult, "url", "status");
+                        char[] removeStuff = { '"', ':', ',' };
+                        string auxgame = getBetween(challengeresult, "id", "url");
+                        string newgame = auxgame.TrimEnd(removeStuff);
+                        game = newgame.TrimStart(removeStuff);
+                        string auxdata = challengedata.TrimEnd(removeStuff);
+                        string newdata = auxdata.TrimStart(removeStuff);
+                        System.Diagnostics.Process.Start(browser, newdata);
+                        await mmic.Send(lce.NewContextRequest());
+                        var exNot = lce.ExtensionNotification(0 + "", 0 + "", 1, challengeresult);
+                        await mmic.Send(exNot);
+                    }
+                }
+                else
+                {
+                    Task<String> accept = Task.Run(() => Accept(game));
+                    string result2 = accept.Result;
+                    string auxok = @"""ok""";
+                    string auxtrue = "true";
+                    string aux = "{" + auxok + ":" + auxtrue + "}";
+                    if (result2.Equals(aux))
+                    {
+                        result2 = "{'ok' : 'Desafio aceite'}";
+                    }
+                    System.Diagnostics.Process.Start(browser, "https://lichess.org/" + game);
+                    await mmic.Send(lce.NewContextRequest());
+                    var exNot = lce.ExtensionNotification(0 + "", 0 + "", 1, result);
+                    await mmic.Send(exNot);
+                }
+            }
         }
+        
         // Function that given a string, a starting point and a ending point, returns the charachters in-between
         public static string getBetween(string strSource, string strStart, string strEnd)
         {
@@ -165,7 +218,7 @@ namespace AppGui
 
             return "";
         }
-
+        
         // Series of Tasks that communicate with the Lichess API to...
         //    ...  move a piece from one to another of given positions
         static async Task<String> MovePiece(String Game, String Move)
@@ -330,6 +383,19 @@ namespace AppGui
                             = new AuthenticationHeaderValue("Bearer", token);
             var result = await client.PostAsync(url, content);
             string resultContent = await result.Content.ReadAsStringAsync();
+            return resultContent;
+        }
+
+        //     ... see if there are games ongoing
+        static async Task<String> SeeCurrentGames()
+        {
+            var client = new HttpClient();
+            var url = new Uri("https://lichess.org/api/account/playing");
+            client.DefaultRequestHeaders.Authorization
+                            = new AuthenticationHeaderValue("Bearer", token);
+            var result = await client.GetAsync(url);
+            string resultContent = await result.Content.ReadAsStringAsync();
+            Console.WriteLine(resultContent);
             return resultContent;
         }
 
